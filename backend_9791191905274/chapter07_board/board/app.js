b886dea6b5268a4eaee073ba161102caa09e9e34
kmin1231 <kmin91143@gmail.com>
2025-01-27 05:43:00 +0900

    🔧 config: 'board' application basic configuration

---
+++ backend_9791191905274/chapter07_board/board/app.js
@@ -0,0 +1,20 @@
+// app.js
+
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", handlebars.engine({
+    layoutsDir: __dirname + "/views",
+    defaultLayout: 'main'
+}))
+
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => {
+    res.render("home", { title: "hello", message: "nice to meet you!" });
+});
+
+app.listen(3000);
