15dedade5150151766752ff54c1778b0caf2138d
kmin1231 <kmin91143@gmail.com>
2025-01-29 06:39:28 +0900

    ✨ feat: add custom helper functions

---
+++ backend_9791191905274/chapter07_board/board/configs/handlebars-helpers.js
@@ -0,0 +1,7 @@
+// handlebars-helpers.js
+
+module.exports = {
+    lengthOfList: (list = []) => list.length,
+    eq: (val1, val2) => val1 === val2,
+    dateString: (isoString) => new Date(isoString).toLocaleDateString(),
+};
