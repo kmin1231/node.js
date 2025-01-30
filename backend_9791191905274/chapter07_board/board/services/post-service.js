f20cbc0ac889dc8835310a2f09fa58ed87255574
kmin1231 <kmin91143@gmail.com>
2025-01-30 10:53:12 +0900

    ✨ feat: add service logic for POST '/write'

---
+++ backend_9791191905274/chapter07_board/board/services/post-service.js
@@ -0,0 +1,11 @@
+// post-service.js
+
+async function writePost(collection, post) {
+    post.hits = 0;
+    post.createdDt = new Date().toISOString();
+    return await collection.insertOne(post);
+}
+
+module.exports = {
+    writePost,
+};
