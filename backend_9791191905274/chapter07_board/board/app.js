610860a02abf4bfc8762842cd10c5cedb6cf1fb9
kmin1231 <kmin91143@gmail.com>
2025-02-04 09:49:14 +0900

    ✨ feat: add API for post modification '/modify'

--- backend_9791191905274/chapter07_board/board/app.js
+++ backend_9791191905274/chapter07_board/board/app.js
@@ -37,7 +37,7 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/write", (req, res) => {
-    res.render("write", { title: "test board" })
+    res.render("write", { title: "test board", mode: "create" });
 });
 
 app.post("/write", async (req, res) => {
@@ -64,6 +64,27 @@ app.post("/check-password", async (req, res) => {
     } else {
         return res.jsoon({ isExist: true });
     }
+});
+
+app.get("/modify/:id", async (req, res) => {
+    const post = await postService.getPostById(collection, req.params.id);
+    console.log(post);
+    res.render("write", { title: "Test Board", "mode": "modify", post });
+});
+
+app.post("/modify/", async (req, res) => {
+    const { id, title, writer, password, content } = req.body;
+
+    const post = {
+        title,
+        writer,
+        password,
+        content,
+        createdDt: new Date().toISOString(),
+    };
+
+    const result = postService.updatePost(collection, id, post);
+    res.redirect(`/detail/${id}`);
 });
 
 
