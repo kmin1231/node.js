e61bc669a3cf309d3a0229da2c14f962b6217e69
kmin1231 <kmin91143@gmail.com>
2025-02-06 14:08:13 +0900

    ✨ feat: add blog controller API

--- backend_9791191905274/chapter08_nestjs/blog/src/blog.controller.ts
+++ backend_9791191905274/chapter08_nestjs/blog/src/blog.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param, Body, Get, Post, Put, Delete } from '@nestjs/common';
 import { BlogService } from './blog.service';
 
-@Controller()
+@Controller("blog")
 export class BlogController {
   constructor(private readonly appService: BlogService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllPosts() {
+    console.log('Fetching all posts');
+  }
+
+  @Post()
+  createPost(@Body() post: any) {
+    console.log('Creating a post');
+    console.log(post);
+  }
+
+  @Get('/:id')
+  getPost(@Param('id') id: string) {
+    console.log(`[id: ${id}] Fetching a single post`);
+  }
+
+  @Delete('/:id')
+  deletePost() {
+    console.log('Deleting a post');
+  }
+
+  @Put('/:id')
+  updatePost(@Param('id') id, @Body() post: any) {
+    console.log(`[${id}] Updating a post`);
+    console.log(post);
   }
 }
