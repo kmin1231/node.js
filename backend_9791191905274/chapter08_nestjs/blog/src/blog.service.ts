dfed6f855828641a01d46c941bf6c3bcb06ce18c
kmin1231 <kmin91143@gmail.com>
2025-02-06 14:17:11 +0900

    ✨ feat: add blog service logic

--- backend_9791191905274/chapter08_nestjs/blog/src/blog.service.ts
+++ backend_9791191905274/chapter08_nestjs/blog/src/blog.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { PostDto } from './blog.model';
 
-@Injectable()
 export class BlogService {
-  getHello(): string {
-    return 'Hello World!';
+  posts = [];
+
+  getAllPosts() {
+    return this.posts;
+  }
+
+  createPost(PostDto: PostDto) {
+    const id = this.posts.length + 1;
+    this.posts.push({ id: id.toString(), ...PostDto, createdDt: new Date() });
+  }
+
+  getPost(id) {
+    const post = this.posts.find((post) => {
+      return post.id === id;
+    });
+    
+    console.log(post);
+    return post;
+  }
+
+  delete(id) {
+    const filteredPosts = this.posts.filter((post) => post.id !== id);
+    this.posts = [...filteredPosts];
+  }
+
+  updatePost(id, postDto: PostDto) {
+    let updateIndex = this.posts.findIndex((post) => post.id === id);
+    const updatePost = { id, ...postDto, updatedDt: new Date() };
+    this.posts[updateIndex] = updatePost;
+    return updatePost;
   }
 }
