5549bfc82a8411c5c8dcfc9f1f45f1e49532abc6
kmin1231 <kmin91143@gmail.com>
2025-02-09 02:04:48 +0900

    🔧 config: set up for MongoDB integration

--- backend_9791191905274/chapter08_nestjs/blog/src/app.module.ts
+++ backend_9791191905274/chapter08_nestjs/blog/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
-import { BlogFileRepository } from './blog.repository';
+import { BlogFileRepository, BlogMongoRepository } from './blog.repository';
+import { Blog, BlogSchema } from './blog.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://minkhu:mongopw@cluster0.gfeye.mongodb.net/blog',
+    ),
+    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+  ],
   controllers: [BlogController],
-  providers: [BlogService, BlogFileRepository],
+  providers: [BlogService, BlogFileRepository, BlogMongoRepository],
 })
 export class AppModule {}
