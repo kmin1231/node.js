540b91161788d134f222d7bbef9de2cc22f77081
kmin1231 <kmin91143@gmail.com>
2025-02-14 04:34:35 +0900

    ✨ feat: implement User controller, service & integrate with repository

--- backend_9791191905274/chapter10_authentication/nest-auth-test/src/user/user.service.ts
+++ backend_9791191905274/chapter10_authentication/nest-auth-test/src/user/user.service.ts
@@ -8,4 +8,34 @@ export class UserService {
     constructor(
         @InjectRepository(User) private userRepository: Repository<User>,
     ) {}
+
+    createUser(user): Promise<User> {
+        return this.userRepository.save(user);
+    }
+
+    async getUser(email: string) {
+        const result = await this.userRepository.findOne({
+            where: { email },
+        });
+
+        // exception handling to prevent null-related TypeError in 'updateUser'
+        if (!result) {
+            throw new Error(`User with email ${email} not found`);
+        }
+
+        return result;
+    }
+
+    async updateUser(email, _user) {
+        const user: User = await this.getUser(email);
+        console.log(_user);
+        user.username = _user.username;
+        user.password = _user.password;
+        console.log(user);
+        this.userRepository.save(user);
+    }
+
+    deleteUser(email: any) {
+        return this.userRepository.delete({ email });
+    }
 }
