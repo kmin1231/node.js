301c26079322a3c07b140bdf03e8a05b66c8b7e1
kmin1231 <kmin91143@gmail.com>
2025-02-21 22:09:08 +0900

    ✨ feat: configure session middleware and implement LocalAuthGuard

--- backend_9791191905274/chapter10_authentication/nest-auth-test/src/auth/auth.guard.ts
+++ backend_9791191905274/chapter10_authentication/nest-auth-test/src/auth/auth.guard.ts
@@ -1,5 +1,7 @@
-import { CanActivate, Injectable } from '@nestjs/common';
+import { CanActivate, Injectable, ExecutionContext, Inject } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class LoginGuard implements CanActivate {
@@ -21,5 +23,26 @@ export class LoginGuard implements CanActivate {
         
         request.user = user;
         return true;
+    }
+}
+
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+    async canActivate(context: any): Promise<boolean> {
+        const result = (await super.canActivate(context)) as boolean;
+
+        const request = context.switchToHttp().getRequest();
+        await super.logIn(request);
+        return result;
+    }
+}
+
+
+@Injectable()
+export class AuthenticatedGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean  {
+        const request = context.switchToHttp().getRequest();
+        return request.isAuthenticated();
     }
 }
