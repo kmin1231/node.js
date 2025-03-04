1b506ae1cef4ec220467a5fe11589b3261082209
kmin1231 <kmin91143@gmail.com>
2025-03-04 15:19:56 +0900

    ✨ feat: implement web socket gateway for messaging

--- backend_9791191905274/chapter13_websocket/nest-chat/src/app.module.ts
+++ backend_9791191905274/chapter13_websocket/nest-chat/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ChatGateway } from './app.gateway';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
