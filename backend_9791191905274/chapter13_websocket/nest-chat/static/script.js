1eb7a11a30fbeaf6f63a67b33ab0e22d7b2b4455
kmin1231 <kmin91143@gmail.com>
2025-03-06 01:51:22 +0900

    ✨ feat: add chat room creation and listing functionality

--- backend_9791191905274/chapter13_websocket/nest-chat/static/script.js
+++ backend_9791191905274/chapter13_websocket/nest-chat/static/script.js
@@ -1,5 +1,8 @@
 const socket = io('http://localhost:3000/chat');
+const rooomSocket = io('http://localhost:3000/room');
 const nickname = prompt('Please enter your nickname!');
+
+let currentRoom = '';
 
 socket.on('connect', () => {
     console.log('connected');
@@ -13,4 +16,18 @@ function sendMessage() {
 
 socket.on('message', (message) => {
     $('#chat').append(`<div>${message}</div>`);
+});
+
+
+function createRoom() {
+    const room = prompt('Please enter the name of the room to create!');
+    rooomSocket.emit('createRoom', { room, nickname });
+}
+
+rooomSocket.on("rooms", (data) => {
+    console.log(data);
+    $('#rooms').empty();
+    data.forEach((room) => {
+        $('#rooms').append(`<li>${room} <button onclick="joinRoom('${room}')">join</button></li>`);
+    });
 });
